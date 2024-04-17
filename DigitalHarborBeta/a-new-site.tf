8b7480886e11c8ff975afc42441a8d943a4d7b7c
pypeaday <github@pype.aleeas.com>
2024-04-16 19:34:48 -0500

    wip

---
+++ DigitalHarborBeta/a-new-site.tf
@@ -0,0 +1,11 @@
+# define stack to provision all the infrastructure
+module "a-new-site" {
+  source      = "../modules/digital-harbor-stack"
+  description = "Stack to provision all the infrastructure for Caribbean Kings"
+
+  repository = "digital-harbor-infrastructure"
+
+  stack_name = "caribbean-kings-beta"
+
+  project_root = "sites/caribbean-kings"
+}
